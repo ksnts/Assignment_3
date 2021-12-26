e73ee4987a9dedc25e319e66c97708245e683012
ksnts <santosjohnkyllem@gmail.com>
2021-12-26 23:54:51 +0800

    First draft

---
+++ assignment_3-2.py
@@ -0,0 +1,16 @@
+def buyO():
+    orange1=int(input("Input amount of oranges: "))
+    return orange1
+
+def buyA():
+    apple1=int(input("Input amount of apples: "))
+    return apple1
+
+def priceO():
+    price1=25
+    return price1
+
+def priceA():
+    price2=20
+    return price2
+
