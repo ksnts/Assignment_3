f47a295099785cc14fb813574a544b28cd5d4310
ksnts <santosjohnkyllem@gmail.com>
2021-12-27 00:05:57 +0800

    Code complete

--- assignment_3-2.py
+++ assignment_3-2.py
@@ -1,3 +1,4 @@
+#functions for amount of oranges and apples
 def buyO():
     orange1=int(input("Input amount of oranges: "))
     return orange1
@@ -5,7 +6,7 @@ def buyO():
 def buyA():
     apple1=int(input("Input amount of apples: "))
     return apple1
-
+#functions for prices
 def priceO():
     price1=25
     return price1
@@ -13,10 +14,18 @@ def priceO():
 def priceA():
     price2=20
     return price2
-
+#main function that multiplies price and amount
 def main_code():
     total1=(buyO()*priceO())
     total2=(buyA()*priceA())
-    
+    main1(total1, total2)
+#function for sum
+def main1(total1, total2):
+    print(f"Total amount is: {total1 +total2} ")
+#call
+main_code()
+
+
+
 
 
