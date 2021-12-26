d5f7bfee28677f538adc9446aead67a18a5bafa3
ksnts <santosjohnkyllem@gmail.com>
2021-12-26 23:43:48 +0800

    Second draft

--- assignment_3.py
+++ assignment_3.py
@@ -11,5 +11,10 @@ def main_code():
     int1=name()
     int2=age()
     int3=address()
-    main1=(int1, int2, int3)
+    main1 (int1, int2, int3)
+    
 
+def main1 (int1, int2, int3):
+    print(f"Hi, my name is {int1}. I am {int2} years old. I live in {int3}.")
+
+main_code()
