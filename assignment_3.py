1e0732de7a2919f722ea498cd08ede11c962751e
ksnts <santosjohnkyllem@gmail.com>
2021-12-26 23:38:02 +0800

    First draft

---
+++ assignment_3.py
@@ -0,0 +1,15 @@
+def name():
+    name1=input("Input your name: ")
+
+def age():
+    age1=input("Input your age: ")
+
+def address():
+    add1=input("Input your address: ")
+
+def main_code():
+    int1=name()
+    int2=age()
+    int3=address()
+    main1=(int1, int2, int3)
+
